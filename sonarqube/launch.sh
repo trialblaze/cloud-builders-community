796e708657b924835edae395c2f5a7f3280cbe09
trialblaze-bt <brian.townsend@trialblaze.com>
2020-06-16 14:06:54 +0800

    feat: allow to specify  env var and cd into that dir to scan

--- sonarqube/launch.sh
+++ sonarqube/launch.sh
@@ -6,5 +6,8 @@ echo "START: Running sonar-scanner-cli on `date`"
 
 sed -i 's/use_embedded_jre=true/use_embedded_jre=false/g' $SONARQUBE_SCANNER_BIN/sonar-scanner
 
+if [[! -z "${GITDIR}" ]]; then
+  cd ${GITDIR}
+fi
 sonar-scanner $@
 echo "END: Running sonar-scanner-cli on `date`"
